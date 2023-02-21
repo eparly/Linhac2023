600be348577f7ca81d5fe082dd1db28205a26bb2
eparly <ethanmparliament@gmail.com>
2023-02-20 23:01:54 -0500

    lpr density function

--- linhac.py
+++ linhac.py
@@ -7,5 +7,20 @@ data = pd.read_csv('Linhac_df_keyed_20_games.csv')
 lpr = data[data['eventname'] == 'lpr']
 xcoords = lpr['xadjcoord']
 ycoords = lpr['yadjcoord']
-plt.plot(xcoords, ycoords)
+
+#plot the density of lpr, pass No. bins to function for resolution
+def lpr_density(bins):
+    heatmap, xedges, yedges = np.histogram2d(xcoords, ycoords, bins=bins)
+
+    # Create extent for imshow
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+    # Plot heatmap
+    plt.clf()
+    plt.imshow(heatmap.T, extent=extent, origin='lower')
+    plt.colorbar()
+    plt.show()
+
+    
+    
 a=0
